2d9fa7b247158ef36981da3053996f5eb4ca4cbc
Cuong Tran <cuongtran92@gmail.com>
2016-02-25 20:58:21 -0500

    edited navbar & login permission

--- app/activity_actions.rb
+++ app/activity_actions.rb
@@ -3,25 +3,31 @@ get '/users/:user_id/trips/:trip_id/activities/:activity_id' do
 end
 
 get '/users/:user_id/trips/:trip_id/activities/new' do
+  check_user_login
   erb :'activities/new'
 end
 
 post '/users/:user_id/trips/:trip_id/activities' do
-  "save new trip"
+  check_user_login
+  "save new activity"
 end
 
 get '/users/:user_id/trips/:trip_id/activities/:activity_id/update' do
+  check_user_login
   erb :'activities/update'
 end
 
 post '/users/:user_id/trips/:trip_id/activities/:activity_id' do
+  check_user_login
   "update trip posted"
 end
 
 get '/users/:user_id/trips/:trip_id/activities/:activity_id/delete' do
+  check_user_login
   erb :'activities/delete'
 end
 
 post '/users/:user_id/trips/:trip_id/activities/:activity_id/delete' do
+  check_user_login
   "delete trip posted"
 end
