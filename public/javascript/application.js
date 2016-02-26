06630cb0b0acf68cdc2b77bd70e287cfd1f73486
ryanplkam <ryanplkam@gmail.com>
2016-02-26 23:40:52 +0000

    Added new activity button

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -17,8 +17,10 @@ $(document).ready(function() {
     // Responsive icons on trip page
     if ($(window).width() < 1200) {
       $(".trip-page .vertical-timeline .glyphicon").css("margin", "8px 0 0 9px");
+      $(".trip-page .vertical-timeline .glyphicon-plus").css("margin", "8px 0 0 11px");
     } else {
       $(".trip-page .vertical-timeline .glyphicon").css("margin", "18px 0 0 19px");
+      $(".trip-page .vertical-timeline .glyphicon-plus").css("margin", "18px 0 0 21px");
     }
 
     // Move buttons on trip page
