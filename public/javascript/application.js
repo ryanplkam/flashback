e1c92edc4cbe71e7c43fbfbf077f78964a3a0a50
ryanplkam <ryanplkam@gmail.com>
2016-02-26 15:28:17 +0000

    Added individual activity page, plus some other tweaks

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+  // // Set height of photos div on inidivual activity page
+  // var raw_height = $(window).height();
+  // var height = raw_height - 51;
+  // $('.activity-photos').height(height);
+
   $(window).resize(function() {
     
     // Responsive sidebar on trips page
@@ -8,7 +13,6 @@ $(document).ready(function() {
     } else {
       $(".trips-page .col-md-4").addClass("fixed-scroll");
     }
-
 
     // Responsive icons on trip page
     if ($(window).width() < 1200) {
@@ -24,6 +28,16 @@ $(document).ready(function() {
     } else {
       $(".trip-page .edit-trip-btn").css("margin", "20px 15px 20px 0px");
       $(".trip-page .edit-trip-btn").addClass("pull-right").removeClass('pull-left');
+    }
+  
+
+     // Responsive sidebar on activity page
+    if ($(window).width() < 992) {
+      $(".activity-details").removeClass("fixed-scroll").css('right', '0');
+      $('.activity-details .col-md-3').removeClass('pull-right').addClass('pull-left');
+    } else {
+      $(".activity-details").addClass("fixed-scroll").css('right', '6%');
+       $('.activity-details .col-md-3').removeClass('pull-left').addClass('pull-right');
     }
 
   });  
