6bed5528362c266214b36192fb6f3338068465ef
Cuong Tran <cuongtran92@gmail.com>
2016-02-26 15:52:21 -0500

    fixed error of wrongly using find_by

--- app/trip_actions.rb
+++ app/trip_actions.rb
@@ -16,7 +16,7 @@ post '/users/:user_id/trips' do
   @trip.end_date = params[:end_date]
   @trip.location = params[:location]
   @trip.description = params[:description]
-  @trip.user = User.find_by(session[:user])
+  @trip.user = User.find_by(id: session[:user])
 
   if @trip.save
     redirect "/users/#{@trip.user_id}/trips"
@@ -25,7 +25,7 @@ post '/users/:user_id/trips' do
   end
 end
 
-get '/users/:user_id/trips/:trip_id/update' do
+get '/users/:uset r_id/trips/:trip_id/update' do
   check_user_login
   @trip = Trip.find_by(id: params[:trip_id])
   check_trip_existense(@trip)
