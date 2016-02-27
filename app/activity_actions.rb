abf7e3823a6bc604b8e924d2f9a8cf0c9ce5c579
Cuong Tran <cuongtran92@gmail.com>
2016-02-26 20:45:43 -0500

    added feature to add new activity

--- app/activity_actions.rb
+++ app/activity_actions.rb
@@ -1,15 +1,35 @@
+get '/users/:user_id/trips/:trip_id/activities/new' do
+  check_user_login
+  @trip = Trip.find_by(id: params[:trip_id])
+  check_user_trip_relationship(@trip)
+
+  erb :'activities/new'
+end
+
 get '/users/:user_id/trips/:trip_id/activities/:activity_id' do
   erb :'activities/index'
 end
 
-get '/users/:user_id/trips/:trip_id/activities/new' do
-  check_user_login
-  erb :'activities/new'
-end
-
 post '/users/:user_id/trips/:trip_id/activities' do
   check_user_login
-  "save new activity"
+  @trip = Trip.find_by(id: params[:trip_id])
+  check_user_trip_relationship(@trip)
+
+  @activity = Activity.new
+  @activity.title = params[:title]
+  @activity.start_date = params[:start_date]
+  @activity.end_date = params[:end_date]
+  @activity.city = params[:city]
+  @activity.country = params[:country]
+  @activity.venue = params[:venue]
+  @activity.description = params[:description]
+  @activity.trip = @trip
+
+  if @activity.save
+    redirect "/users/#{@activity.trip.user_id}/trips/#{@activity.trip_id}"
+  else
+    erb :'activities/new'
+  end
 end
 
 get '/users/:user_id/trips/:trip_id/activities/:activity_id/update' do
