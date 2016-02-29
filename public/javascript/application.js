00cbc117090e7555540570fb7c36c935b06ab6d3
ryanplkam <ryanplkam@gmail.com>
2016-02-29 04:15:14 +0000

    Major responsive tweaks

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,12 +1,6 @@
 $(document).ready(function() {
 
-  if ($(window).width() < 1170) {
-    $(".trips-page .col-lg-4").removeClass("fixed-scroll");
-  } else {
-    $(".trips-page .col-lg-4").addClass("fixed-scroll");
-  }
-
-  // Responsive icons on trip page
+  // Responsive icons on trip page OLD 
   if ($(window).width() < 1200) {
     $(".trip-page .vertical-timeline .glyphicon").css("margin", "8px 0 0 9px");
     $(".trip-page .vertical-timeline .glyphicon-plus").css("margin", "8px 0 0 11px");
@@ -14,34 +8,17 @@ $(document).ready(function() {
     $(".trip-page .vertical-timeline .glyphicon").css("margin", "18px 0 0 19px");
     $(".trip-page .vertical-timeline .glyphicon-plus").css("margin", "18px 0 0 21px");
   }
-
-  // Move buttons on trip page
-  if ($(window).width() < 992) {
-    $(".trip-page .edit-trip-btn").css("margin", "5px 15px 15px 0px");
-    $(".trip-page .edit-trip-btn").removeClass("pull-right").addClass('pull-left');
-  } else {
-    $(".trip-page .edit-trip-btn").css("margin", "20px 0px 20px 15px");
-    $(".trip-page .edit-trip-btn").addClass("pull-right").removeClass('pull-left');
-  }
   
-  // Responsive buttons on activity page
-  if ($(window).width() < 1200) {
-    $('.activity-page .btn-group, .back-button').removeClass('pull-right').addClass('pull-left')
-    $('.activity-page .btn-group').css('margin', '0 15px 0 0')
-    $('.activity-page .btn-group .btn').css('margin', '0')
-    $('.activity-page .back-button').css('margin', '0 0 15px 0') 
-  }  
+  // Responsive justified buttons on trip page
+  if ($(window).width() < 768) {
+    $('.edit-trip-btns').addClass('btn-group').addClass('btn-group-justified');
+  } else {
+    $('.edit-trip-btns').removeClass('btn-group').removeClass('btn-group-justified');
+  }
 
   $(window).resize(function() {
     
-    // Responsive sidebar on trips page
-    if ($(window).width() < 1200) {
-      $(".trips-page .col-lg-4").removeClass("fixed-scroll");
-    } else {
-      $(".trips-page .col-lg-4").addClass("fixed-scroll");
-    }
-
-    // Responsive icons on trip page
+    // Responsive icons on trip page OLD
     if ($(window).width() < 1200) {
       $(".trip-page .vertical-timeline .glyphicon").css("margin", "8px 0 0 9px");
       $(".trip-page .vertical-timeline .glyphicon-plus").css("margin", "8px 0 0 11px");
@@ -50,22 +27,11 @@ $(document).ready(function() {
       $(".trip-page .vertical-timeline .glyphicon-plus").css("margin", "18px 0 0 21px");
     }
 
-    // Move buttons on trip page
-    if ($(window).width() < 992) {
-      $(".trip-page .edit-trip-btn").css("margin", "5px 15px 15px 0px");
-      $(".trip-page .edit-trip-btn").removeClass("pull-right").addClass('pull-left');
+    // Responsive justified buttons on trip page
+    if ($(window).width() < 768) {
+      $('.edit-trip-btns').addClass('btn-group').addClass('btn-group-justified');
     } else {
-      $(".trip-page .edit-trip-btn").css("margin", "20px 0px 20px 15px");
-      $(".trip-page .edit-trip-btn").addClass("pull-right").removeClass('pull-left');
-    }
-
-    if ($(window).width() < 1200) {
-      $('.activity-page .btn-group, .back-button').removeClass('pull-right').addClass('pull-left')
-      $('.activity-page .btn-group').css('margin', '0 15px 0 0')
-      $('.activity-page .btn-group .btn').css('margin', '0')
-      $('.activity-page .back-button').css('margin', '0 0 15px 0')
-    } else {
-      $('.activity-page .btn-group, .back-button').removeClass('pull-left').addClass('pull-right').css('margin', '20px 0 0 15px')
+      $('.edit-trip-btns').removeClass('btn-group').removeClass('btn-group-justified');
     }
 
   });  
